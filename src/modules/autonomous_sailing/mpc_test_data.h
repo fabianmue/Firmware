f85f20be87f82b9f2aa20e1a16005f07d89adc96
Femer <femer90@hotmail.it>
2015-02-25 16:32:53 +0100

    Utility to test mpc

---
+++ src/modules/autonomous_sailing/mpc_test_data.h
@@ -0,0 +1,16 @@
+#ifndef MPC_TEST_DATA_H
+#define MPC_TEST_DATA_H
+
+#include <stdint.h>
+#include "settings.h"
+#include "guidance_module.h"
+
+void mt_init_mpc_data(void);
+
+void mt_get_next_yr_y(float x[3]);
+
+bool mt_still_data(void);
+
+float mt_get_correct_rud(void);
+
+#endif // MPC_TEST_DATA_H
