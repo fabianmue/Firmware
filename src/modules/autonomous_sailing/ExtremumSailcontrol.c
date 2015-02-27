1f2572363faad1a6d0c60a633bd6491e0e1d3398
wirzjo <wirzjo@student.ethz.ch>
2015-02-27 07:18:55 +0100

    Created new c/h files

---
+++ src/modules/autonomous_sailing/ExtremumSailcontrol.c
@@ -0,0 +1,18 @@
+/*
+ * ExtremumSailcontrol.c
+ *
+ *  Extremum Seeking Sail Control (ESSC) is an algorithm to find the optimal sail angle for maximizing the speed of the boat.
+ *  It is based on the approach presented in the following paper: TODO
+ *
+ *
+ *  Created on: 26.02.2015
+ *      Author: Jonas Wirz (wirzjo@student.ethz.ch)
+ */
+
+
+
+
+
+
+
+
--- src/modules/autonomous_sailing/autonomous_sailing.c
+++ src/modules/autonomous_sailing/autonomous_sailing.c
@@ -278,6 +278,8 @@ int as_daemon_thread_main(int argc, char *argv[]){
                     //look into optimal path planning maps for reference actions
                     path_planning(&ref_act, &strs);
 
+                    ExtremumSailcontrol(&strs);
+
                     //update NED velocities in navigation module
                     //update_ned_vel(strs.gps_filtered.vel_n, strs.gps_filtered.vel_e, strs.gps_filtered.vel_d);
 
