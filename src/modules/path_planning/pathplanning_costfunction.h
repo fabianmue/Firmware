904f680036d5f3d0d5b6afe12594fd3ba1bfdb23
wirzjo <wirzjo@student.ethz.ch>
2015-03-03 15:11:39 +0100

    added files to new Pathplanning module

---
+++ src/modules/path_planning/pathplanning_costfunction.h
@@ -0,0 +1,51 @@
+/*
+ * pathplanning_costfunction.h
+ *
+ * This file contains all functions/variables for calculating an optimal path to a given target position.
+ *
+ *  Created on: 02.03.2015
+ *      Author: Jonas
+ */
+
+#ifndef PATHPLANNING_COSTFUNCTION_H_
+#define PATHPLANNING_COSTFUNCTION_H_
+
+
+#include <math.h>
+#include <stdint.h>
+
+#include "topics_handler.h"
+
+
+/* @brief Set a new target point to be reached from QGround Control */
+void ppc_set_target(float lat, float lon);
+
+
+/* @brief Set the position of the obstacle */
+void ppc_set_obstacle(float lat, float lon);
+
+
+/* @brief Set the configuration Parameters from QGround Control */
+void ppc_set_configuration(float Gw, float Go, float Gm, float Gs, float Gt, float GLee, float ObstSafetyRadius, float ObstHorizon);
+
+
+/* @brief Update the Winddirection from the Weatherstation */
+void ppc_update_WSAI(const struct structs_topics_s *strs_p);
+
+
+/* @brief Update the Vehicles Position based on GPS Data */
+void ppc_update_GPOS(const struct structs_topics_s *strs_p);
+
+
+/* @brief Update the Vehicles Heading */
+void ppc_update_HEADING(const struct structs_topics_s *strs_p);
+
+
+/* @brief Calculate the optimal heading and give corresponding command to helsman*/
+void navigator(void);
+
+
+
+
+
+#endif /* PATHPLANNING_COSTFUNCTION_H_ */
---
+++ src/modules/path_planning/topics_handler.h
@@ -0,0 +1,73 @@
+#ifndef TOPICS_HANDLER_H
+#define TOPICS_HANDLER_H
+
+#include <uORB/uORB.h>
+#include <uORB/topics/actuator_controls.h>
+#include <uORB/topics/actuator_armed.h>
+
+#include <uORB/topics/wind_sailing.h>
+#include <uORB/topics/vehicle_global_position.h>
+#include <uORB/topics/boat_weather_station.h>
+#include <uORB/topics/vehicle_gps_position.h>
+#include <uORB/topics/boat_guidance_debug.h>
+#include <uORB/topics/parameter_update.h>
+#include <uORB/topics/vehicle_attitude.h>
+#include <uORB/topics/boat_opt_status.h>
+#include <uORB/topics/boat_qgc_param.h>
+#include <uORB/topics/boat_optimal_control.h>
+#include <uORB/topics/rc_channels.h>
+
+//only for debug on qcg
+#include <uORB/topics/airspeed.h>
+
+struct subscribtion_fd_s{
+    int gps_raw;
+    int gps_filtered;
+    int wind_sailing;
+    int parameter_update;
+    int att;
+    int boat_weather_station;
+    int rc_channels;
+};
+
+struct published_fd_s{
+    orb_advert_t actuator_pub;
+    orb_advert_t boat_guidance_debug_pub;
+    orb_advert_t boat_opt_status;
+    orb_advert_t boat_qgc_param1;
+    orb_advert_t boat_qgc_param2;
+    orb_advert_t boat_opt_mat;
+
+    orb_advert_t boat_qgc_param3;
+    //only for debug on qGC
+    orb_advert_t airspeed;
+};
+
+struct structs_topics_s{
+   struct actuator_controls_s actuators;
+   struct vehicle_gps_position_s gps_raw;
+   struct vehicle_global_position_s gps_filtered;
+   struct wind_sailing_s wind_sailing;
+   struct boat_guidance_debug_s boat_guidance_debug;
+   struct parameter_update_s update;
+   struct vehicle_attitude_s att;
+   struct rc_channels_s rc_channels;
+   struct boat_opt_status_s boat_opt_status;
+   struct boat_qgc_param1_s boat_qgc_param1;
+   struct boat_qgc_param2_s boat_qgc_param2;
+   struct boat_opt_mat_s boat_opt_mat;
+   //struct boat_opt_ctr_s boat_opt_ctr;
+
+   //bool boat_opt_control_updated;
+   bool boat_opt_status_updated;
+
+   struct boat_qgc_param3_s boat_qgc_param3;
+
+   //only for debug on qGC
+   struct airspeed_s airspeed;
+};
+
+
+
+
+#endif // TOPICS_HANDLER_H
