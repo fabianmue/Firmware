9dc3c4b26d21004a4539f9d4bcb092b71d8e7721
wirzjo <wirzjo@student.ethz.ch>
2015-03-23 08:01:28 +0100

    not working version

--- src/modules/path_planning/pp_navigator.c
+++ src/modules/path_planning/pp_navigator.c
@@ -20,6 +20,7 @@
  * - add Potentialfield Method
  * - Winddirection => how's the definition? Wind from Nort = 0°/ Wind from South = 180° (Sensor-Frame)
  * - Check E7-Convention (North must have 9 Digits!!!!) 470000000
+ * - Add logging of Pathplanning parameters
  */
 
 
