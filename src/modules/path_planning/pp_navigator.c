495c79c86cb82be173c71112330774b38cbeb3f6
wirzjo <wirzjo@student.ethz.ch>
2015-03-24 15:06:40 +0100

    small changes, but still problem with alpha_ref

--- src/modules/path_planning/pp_navigator.c
+++ src/modules/path_planning/pp_navigator.c
@@ -325,7 +325,7 @@ void nav_speak2helsman() {
 
 	//Report the Result of the Pathplanning to QGround Control
 	//sprintf(txt_msg, "New Alpha Star = %1.1f [deg], Maneuver = %d @ %d" , (double)(alpha_star*RAD2DEG),(int)(state.maneuver),(int)state.last_call);
-	sprintf(txt_msg, "New Alpha Star = %1.1f [deg], Maneuver = %d @ %d" , (double)(state.heading_ref),(int)(state.maneuver),(int)state.last_call);
+	sprintf(txt_msg, "New Alpha Star = %1.1f [deg], Maneuver = %d @ %d" , (double)(state.heading_ref*RAD2DEG),(int)(state.maneuver),(int)state.last_call);
 	smq_send_log_info(txt_msg);
 }
 
