506b9469f64d6b9216eac0b5b8a95e6012a21394
wirzjo <wirzjo@student.ethz.ch>
2015-03-30 14:52:27 +0200

    added files for failsafe-mode

---
+++ src/modules/path_planning/pp_failsafe.c
@@ -0,0 +1,135 @@
+/*
+ * pp_failsafe.c
+ *
+ * This file contains functions that are used for failsafe.
+ * A home-position is defined. As soon as the RC-Signal is lost, the boat
+ * returns fully autonomously to this home-position (by Avoiding Obstacles and minimizing time to target :) )
+ *
+ *  Created on: 30.03.2015
+ *      Author: Jonas Wirz <wirzjo@student.ethz.ch>
+ */
+
+
+#include <stdbool.h>
+#include <drivers/drv_hrt.h>
+
+#include "pp_failsafe.h"
+
+
+#define SAFETYTIME_US  10e6 	//Time for which the RC-Signal must be lost before the failsafe is switched to ACTIVE [us]
+
+
+
+typedef enum {INACTIVE, ACTIVE, SAFETYTIME} STATE_enum;
+static STATE_enum SM_State = INACTIVE;
+
+static uint64_t on_time = 0;	//Timestamp, when the RC-Signal was lost the first time
+
+static bool lost_signal = false;		//true, iff the signal is lost, false else
+
+
+
+/**
+ * Init the failsafe-mode
+ *
+ */
+void fs_init(void) {
+	SM_State = INACTIVE; 		//By default the failsafe is off (inactive)
+	on_time = 0;
+	lost_signal = false; 		//By default we assume that a signal is present
+}
+
+
+
+/**
+ * Is failsafe mode active?
+ *
+ * @return true, if the rc_signal is lost and the failsafe mode is active
+ */
+bool fs_is_failsafe_active(void) {
+	if(SM_State == ACTIVE) {
+		return true;
+	} else {
+		return false;
+	}
+
+}
+
+
+
+/**
+ * Get the state of the RC-Signal (Signal-Lost-Detection)
+ *
+ * This function sets the state of the failsafe mode
+ */
+void fs_check_rc_signal(struct structs_topics_s *strs_p) {
+
+	if(strs_p->rc_channels.signal_lost == true) {
+		//We lost the signal => update internal signal state
+		lost_signal = true;
+	} else {
+		lost_signal = false;
+	}
+
+}
+
+
+
+/**
+ * Run the state-machine
+ *
+ * Note: This function should be executed in every main-while-loop cycle
+ */
+void fs_state_machine() {
+	//**** INACTIVE - state
+	if(SM_State == INACTIVE) {
+		if(lost_signal == true) {
+			//The signal is lost => we switch to SAFETYTIME - state
+			SM_State = SAFETYTIME;
+			on_time = hrt_absolute_time();
+		}
+	}
+
+
+	//**** SAFETYTIME - state
+	if(SM_State == SAFETYTIME) {
+		if(lost_signal == false) {
+			//we regained the signal => we can switch to INACTIVE - state
+			SM_State = INACTIVE;
+			return;	//Do not loose anymore time and return to INACTIVE
+		}
+
+		if((hrt_absolute_time() - on_time) >= SAFETYTIME_US) {
+			//the signal is lost longer than the safetytime => we can switch to ACTIVE - state
+			SM_State = ACTIVE;
+		} else {
+			SM_State = SAFETYTIME;
+		}
+	}
+
+
+	//**** ACTIVE - state
+	if(SM_State == ACTIVE) {
+
+		if(lost_signal == false) {
+			//We regained the signal => we can switch to INACTIVE - state
+			SM_State = INACTIVE;
+			return; //Do not loose anymore time and return to INACTIVE
+		}
+
+		if(lost_signal == true) {
+			//We still have no signal => we stay in ACTIVE - state
+			SM_State = ACTIVE;
+
+			//Note: It could be that we reach the target (home-position), but we do not care,
+			//		since even if we reach the target we want to stay there until we have a
+			//      valid RC-Signal. => the only way of getting back into the INACTIVE - state
+			//      is by regaining the RC-Signal.
+		}
+
+	}
+}
+
+
+
+
---
+++ src/modules/path_planning/pp_failsafe.h
@@ -0,0 +1,31 @@
+/*
+ * pp_failsafe.h
+ *
+ *  Created on: 30.03.2015
+ *      Author: Jonas
+ */
+
+#ifndef PP_FAILSAFE_H_
+#define PP_FAILSAFE_H_
+
+
+#include "pp_topics_handler.h"
+
+
+/* @brief Init the use of the failsafe */
+void fs_init(void);
+
+
+/* @brief Check, if the failsafe is active */
+bool fs_is_failsafe_active(void);
+
+
+/* @brief Check, if an RC-Signal is present */
+void fs_check_rc_signal(struct structs_topics_s *strs_p);
+
+
+/* @brief Main state-machine that is executed in the main-while-loop */
+void fs_state_machine();
+
+
+#endif /* PP_FAILSAFE_H_ */
