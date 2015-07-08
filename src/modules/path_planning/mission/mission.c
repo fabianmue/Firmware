1f49b044be92239791abf689020fed89190e1f08
wirzjo <wirzjo@student.ethz.ch>
2015-07-08 16:48:09 +0200

    added mission 5, which has a 1min countdown prior to start

--- src/modules/path_planning/mission/mission.c
+++ src/modules/path_planning/mission/mission.c
@@ -38,8 +38,12 @@
 #include <math.h>
 #include "mission.h"
 
+#include <drivers/drv_hrt.h>
+
 #include "../pp_navigation_helper.h"
 #include "../pp_navigator.h"
+#include "../pp_send_msg_qgc.h"
+#include "../pp_communication_buffer.h"
 #include "../pp_send_msg_qgc.h"
 
 
@@ -69,6 +73,12 @@ static struct {
 	.curtask = 0
 };
 
+
+static uint64_t lastcall = 0; //Timestamp of the last call to the handler-function
+static bool auto_mode_on = false; //True, if the autonomous mode is on
+
+static bool countdown = false; //Set true, if a countdown is needed
+static uint64_t countdown_ms = 0; //Number of milliseconds for the countdown
 
 //static char txt_msg[150]; //Buffer for QGround Control Messages
 
@@ -261,6 +271,57 @@ bool mi_set_new_task(uint8_t tasknum) {
 			break;
 		}
 
+		case 5: { //Competition race course without any obstacles
+
+			//The course is: Start -> Q1 -> Q4 -> Finish
+
+			//First we define a countdown. During the time of the countdown we sail between two buoys behind the start-line
+			if(countdown == false) {
+				countdown = true;
+				countdown_ms = 60e6; //Set the countdown to 1min
+
+				smq_send_log_info("Started Countdown");
+
+				NEDpoint start1;
+				NEDpoint start2;
+
+				start1.northx = O3.northx - 6.0f;
+				start1.easty = O3.easty + 6.0f;
+				start2.northx = O4.northx - 6.0f;
+				start2.easty = O4.easty - 6.0f;
+
+				start1 = nh_rotate(start1, O1, config.rotation);
+				start2 = nh_rotate(start2, O1, config.rotation);
+
+				nav_set_target_ned(0,start1);
+				nav_set_target_ned(1,start2);
+			}
+
+
+			//The countdown is over => we can start
+			if(countdown_ms == 0) {
+				NEDpoint end;
+
+				end.northx = O3.northx + config.dist/4;
+				end.easty = O3.easty-3;
+
+				//Rotate around Point O1
+				O3 = nh_rotate(O3, O1, config.rotation);
+				O4 = nh_rotate(O4, O1, config.rotation);
+				end = nh_rotate(end,O1,config.rotation);
+
+				//Set the configuration
+				nav_set_target_ned(0, O1);
+				nav_set_target_ned(1, O4);
+				nav_set_target_ned(2, end);
+
+
+			}
+
+
+			break;
+		}
+
 		default: {
 			//Nothing we can do about this
 			break;
@@ -271,6 +332,80 @@ bool mi_set_new_task(uint8_t tasknum) {
 	//Everything is OK => return true
 	return true;
 }
+
+
+/*
+ * Periodically called function by the main-program loop
+ *
+ */
+bool mi_handler(void) {
+
+	uint64_t curtime = hrt_absolute_time();
+
+	if(curtime-lastcall > 2e6) {
+		//Do not call the mission planner every time in the main-loop, but only every two seconds
+
+		bool just_switched = false; //Flag that signals when we just switched into autonomous mode
+
+		if(auto_mode_on == false && cb_is_autonomous_mode() == true) {
+			//We just switched into autonomous mode
+
+			auto_mode_on = true; //Signal that we are in autonomous mode now
+			just_switched = true;
+		}
+
+		if(cb_is_autonomous_mode() == false) {
+			//We are in manual mode => clear the flag
+
+			auto_mode_on = false;
+		}
+
+		if(cb_is_autonomous_mode() == true) {
+			//we are in autonomous mode => set the flag
+
+			auto_mode_on = true;
+
+			if(countdown == true) {
+				//A countdown is set
+
+				if(countdown_ms == 0) {
+					//The countdown is over => set countdown-flag to false and start original mission
+					countdown = false;
+
+					smq_send_log_info("End of Countdown (JW)");
+
+					countdown_ms = 0;
+					mi_set_new_task(state.curtask);
+				}
+
+				if(countdown == true) {
+					countdown_ms -= 2e6; //Decrease the countdown by two seconds
+				} else {
+					countdown_ms = 0;
+				}
+			}
+		}
+
+
+
+		//We've just switched into autonomous mode => start the countdown
+		if(just_switched == true) {
+			//We've just switched into autonomous mode => start the countdown!
+
+
+
+		}
+
+
+
+
+	} //if lastcall
+
+
+
+
+}
+
 
 
 
